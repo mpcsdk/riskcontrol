a7a6b80ae462cbd50f971e8aa454a62a4b5ebc06
frank <no>
2023-09-22 18:32:59 +0800

    optimize

--- internal/logic/tfa/verify.go
+++ internal/logic/tfa/verify.go
@@ -5,16 +5,15 @@ import (
 	"riskcontral/internal/consts"
 
 	"github.com/gogf/gf/v2/errors/gerror"
-	"github.com/gogf/gf/v2/frame/g"
 )
 
 func (s *sTFA) VerifyCode(ctx context.Context, userId string, riskSerial string, code string) error {
-	_, err := s.TFAInfo(ctx, userId)
-	if err != nil {
-		g.Log().Warning(ctx, "VerifyCode:", userId, riskSerial, err)
-		return gerror.NewCode(consts.CodeTFANotExist)
-	}
-	// 验证验证码
+	// _, err := s.TFAInfo(ctx, userId)
+	// if err != nil {
+	// 	g.Log().Warning(ctx, "VerifyCode:", userId, riskSerial, err)
+	// 	return gerror.NewCode(consts.CodeTFANotExist)
+	// }
+	// // 验证验证码
 
 	key := s.verifyPenddingKey(userId, riskSerial, code)
 	if task, ok := s.verifyPendding[key]; ok {
